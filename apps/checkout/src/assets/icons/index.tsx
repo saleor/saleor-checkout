92be5f821dc6add587ca78e8b913a4d18fe1a170
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-02-01 17:57:13 +0100

    Move icons to assets directory

---
+++ apps/checkout/src/assets/icons/index.tsx
@@ -0,0 +1,8 @@
+import AddIcon from "./add.svg";
+import BackIcon from "./back.svg";
+import AlertIcon from "./alert.svg";
+import ChevronIcon from "./chevron.svg";
+import PlusIcon from "./plus.svg";
+import MinusIcon from "./minus.svg";
+
+export { AddIcon, BackIcon, AlertIcon, ChevronIcon, PlusIcon, MinusIcon };
