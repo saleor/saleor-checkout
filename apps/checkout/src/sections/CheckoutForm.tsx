0ec0db1bf6b05755ee18541b8b913978ad11d8bb
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-02-04 14:28:09 +0100

    Change px to rem for styling font size and spacing

--- apps/checkout/src/sections/CheckoutForm.tsx
+++ apps/checkout/src/sections/CheckoutForm.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Text } from "@components/Text";
 
 interface CheckoutFormProps {}
 
 const CheckoutForm: React.FC<CheckoutFormProps> = ({}) => {
   return (
     <div className="checkout-form" style={{ border: "1px solid red" }}>
-      Checkout here
+      <Text>Checkout here</Text>
     </div>
   );
 };
--- apps/checkout/src/sections/Summary.tsx
+++ apps/checkout/src/sections/Summary.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Text } from "@components/Text";
 
 interface SummaryProps {}
 
 const Summary: React.FC<SummaryProps> = ({}) => {
   return (
     <div className="summary" style={{ border: "1px solid blue" }}>
-      Summary here
+      <Text>Summary here</Text>
     </div>
   );
 };
