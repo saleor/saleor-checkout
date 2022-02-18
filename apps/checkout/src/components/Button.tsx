0bfcb94f95cbfe132eb76d4ea0696c29476203c2
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-02-18 10:24:19 +0100

    Refine page not found, add primary and secondary buttons

--- apps/checkout/src/components/Button.tsx
+++ apps/checkout/src/components/Button.tsx
@@ -1,30 +1,40 @@
 import React, { ElementType } from "react";
 import { useButton } from "@react-aria/button";
 import { useRef } from "react";
-import { Text, TextProps } from "@components/Text";
+import { Text } from "@components/Text";
 import { AriaButtonProps } from "@react-types/button";
 import clsx from "clsx";
+import { Classes } from "@lib/globalTypes";
 
-interface ButtonProps extends AriaButtonProps<ElementType<HTMLButtonElement>> {
+interface ButtonProps
+  extends AriaButtonProps<ElementType<HTMLButtonElement>>,
+    Classes {
+  disabled?: boolean;
   variant?: "primary" | "secondary";
   title: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
   title,
+  className,
   variant = "primary",
+  disabled = false,
   ...rest
 }) => {
   const ref = useRef<HTMLButtonElement | null>(null);
   const { buttonProps } = useButton(rest, ref);
 
-  const classes = clsx("button", {
-    "button-primary": variant === "primary",
-    "button-secondary": variant === "secondary",
-  });
+  const classes = clsx(
+    "button",
+    {
+      "button-primary": variant === "primary",
+      "button-secondary": variant === "secondary",
+    },
+    className
+  );
 
   return (
-    <button className={classes} ref={ref} {...buttonProps}>
+    <button {...buttonProps} disabled={disabled} className={classes} ref={ref}>
       <Text>{title}</Text>
     </button>
   );
