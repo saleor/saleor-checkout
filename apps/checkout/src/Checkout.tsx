f09c77e637cfc53aa90815ebcb692425541aa7ee
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-02-18 10:24:19 +0100

    Refine page not found, add primary and secondary buttons

--- apps/checkout/src/Checkout.tsx
+++ apps/checkout/src/Checkout.tsx
@@ -5,26 +5,29 @@ import { Suspense } from "react";
 import { SummaryPlaceholder } from "@sections/Summary/SummaryPlaceholder";
 import { extractTokenFromUrl } from "@lib/utils";
 import { PageNotFound } from "@sections/PageNotFound";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const Checkout = () => {
   const token = extractTokenFromUrl();
 
   return (
     <div className="app">
-      {token ? (
-        <div className="page">
-          <PageHeader />
-          <div className="page-content">
-            <CheckoutForm />
-            <div className="page-divider" />
-            <Suspense fallback={<SummaryPlaceholder />}>
-              <Summary />
-            </Suspense>
+      <ErrorBoundary FallbackComponent={PageNotFound}>
+        {token ? (
+          <div className="page">
+            <PageHeader />
+            <div className="page-content">
+              <CheckoutForm />
+              <div className="page-divider" />
+              <Suspense fallback={<SummaryPlaceholder />}>
+                <Summary />
+              </Suspense>
+            </div>
           </div>
-        </div>
-      ) : (
-        <PageNotFound />
-      )}
+        ) : (
+          <PageNotFound />
+        )}
+      </ErrorBoundary>
     </div>
   );
 };
--- apps/checkout/src/sections/PageNotFound.tsx
+++ apps/checkout/src/sections/PageNotFound.tsx
@@ -1,23 +1,31 @@
 import { useFormattedMessages } from "@hooks/useFormattedMessages";
 import { Text } from "@components/Text";
-import React from "react";
 import Button from "@components/Button";
 import { SaleorLogo } from "@images";
 
 export const PageNotFound = () => {
   const formatMessage = useFormattedMessages();
 
+  // eslint-disable-next-line no-restricted-globals
+  const goBack = () => history.back();
+
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center pt-12">
       <div className="w-full flex justify-center">
         <img src={SaleorLogo} alt="logo" className="logo" />
       </div>
-      <div className="h-full flex flex-col items-center justify-center mb-22 max-w-80">
-        <Text title className="mb-4">
+      <div className="h-full flex flex-col items-center justify-center mb-22">
+        <Text title className="mb-4 text-center">
           {formatMessage("problemTitle")}
         </Text>
-        <Text className="mb-4">{formatMessage("problemDescription")}</Text>
-        <Button title={formatMessage("goBackToStore")} />
+        <Text className="mb-6 max-w-85 text-center">
+          {formatMessage("problemDescription")}
+        </Text>
+        <Button
+          onPress={goBack}
+          variant="secondary"
+          title={formatMessage("goBackToStore")}
+        />
       </div>
     </div>
   );
