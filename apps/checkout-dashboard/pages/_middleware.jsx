7f5f2fbf9cb3a26391a0d59b160c98fe784e30b1
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-02-21 11:14:26 +0100

    Use native redirect API

--- apps/checkout-dashboard/pages/_middleware.jsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function middleware(req) {
-  const { pathname } = req.nextUrl;
-  if (pathname == "/") {
-    return NextResponse.redirect("/channels");
-  }
-  return NextResponse.next();
-}
