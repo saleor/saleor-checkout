f07c57e9c5052bdb5c352d8e0e278d1f11ba4f9e
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-02-23 18:15:00 +0100

    Add using token from env for preview purposes

--- apps/checkout/src/lib/utils.ts
+++ apps/checkout/src/lib/utils.ts
@@ -3,9 +3,9 @@ import queryString from "query-string";
 export const extractTokenFromUrl = (): string => {
   const token =
     // eslint-disable-next-line no-restricted-globals
-    (queryString.parse(location.search) as { token?: string | null })?.token;
-  // for development & preview purposes
-  // || process.env.REACT_APP_CHECKOUT_TOKEN
+    (queryString.parse(location.search) as { token?: string | null })?.token ||
+    // for development & preview purposes
+    process.env.REACT_APP_CHECKOUT_TOKEN;
 
   if (typeof token !== "string") {
     throw "Checkout token does not exist";
