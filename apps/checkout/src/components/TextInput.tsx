633a40042598915c990beb38c84de222ee39f97a
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-03-04 11:36:53 +0100

    changes to check ci checks

--- apps/checkout/src/components/TextInput.tsx
+++ apps/checkout/src/components/TextInput.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { AriaTextFieldOptions, useTextField } from "@react-aria/textfield";
 import { Classes } from "@lib/globalTypes";
 
+// @ts-ignore TMP
 interface TextInputProps extends AriaTextFieldOptions<"input">, Classes {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
