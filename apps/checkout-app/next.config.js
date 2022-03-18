8a99aea2b5b4905733ad81a62aabfcb7b21327e5
Jakub Majorek <majorek.jakub@gmail.com>
2022-03-18 13:51:43 +0100

    Restructure project

---
+++ apps/checkout-app/next.config.js
@@ -0,0 +1,19 @@
+const withTM = require("next-transpile-modules")(["@saleor/checkout"]);
+
+module.exports = withTM({
+  i18n: {
+    locales: ["en-US", "pl-PL"],
+    defaultLocale: "en-US",
+  },
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/channels",
+        permanent: true,
+      },
+    ];
+  },
+  images: { domains: ["localhost"] },
+});
--- apps/checkout-dashboard/api/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "urql";
-
-export const client = createClient({
-  url: process.env.API_URL,
-});
