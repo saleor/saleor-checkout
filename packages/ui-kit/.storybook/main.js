aee7de7ba028eea8c028b28f18d18fb094377bbe
Adrian Potępa <adrian.potepa@gmail.com>
2022-03-28 13:39:15 +0200

    Added config to handle css modules in storybook preview

--- packages/ui-kit/.storybook/main.js
+++ packages/ui-kit/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -17,4 +19,32 @@ module.exports = {
     },
   ],
   framework: "@storybook/react",
+  webpackFinal: async (config, options) => {
+    config.module.rules.find(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    ).exclude = /\.module\.css$/;
+
+    config.module.rules.push({
+      test: /\.module\.css$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+          },
+        },
+        {
+          loader: "postcss-loader",
+          options: {
+            postcssOptions: {
+              plugins: [require("tailwindcss")(), require("autoprefixer")()],
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
--- packages/ui-kit/src/components/Button/Button.tsx
+++ packages/ui-kit/src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { FC, ButtonHTMLAttributes } from "react";
 import clsx from "clsx";
 
+import styles from "./Button.module.css";
 import { Text } from "../Text";
 
 export interface ButtonProps extends ButtonHTMLAttributes<{}> {
@@ -16,11 +17,11 @@ export const Button: FC<ButtonProps> = ({
   ...rest
 }) => {
   const classes = clsx(
-    "button",
+    styles.button,
     {
-      "button-primary": variant === "primary",
-      "button-secondary": variant === "secondary",
-      "button-tertiary": variant === "tertiary",
+      [styles["button-primary"]]: variant === "primary",
+      [styles["button-secondary"]]: variant === "secondary",
+      [styles["button-tertiary"]]: variant === "tertiary",
     },
     className
   );
