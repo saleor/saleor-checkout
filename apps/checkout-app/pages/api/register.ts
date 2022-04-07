3f402cdfefd8362acb4174d409531b7b312a0164
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-04-07 19:04:27 +0200

    Add missing register endpoint

---
+++ apps/checkout-app/pages/api/register.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { SALEOR_DOMAIN_HEADER } from "../../constants";
+
+const handler = async (
+  request: NextApiRequest,
+  response: NextApiResponse
+): Promise<undefined> => {
+  console.log(request);
+
+  const saleor_domain = request.headers[SALEOR_DOMAIN_HEADER];
+  if (!saleor_domain) {
+    response
+      .status(400)
+      .json({ success: false, message: "Missing saleor domain token." });
+    return;
+  }
+
+  const auth_token = request.body?.auth_token as string;
+  if (!auth_token) {
+    response
+      .status(400)
+      .json({ success: false, message: "Missing auth token." });
+    return;
+  }
+
+  await fetch(process.env.SALEOR_MARKETPLACE_REGISTER_URL as string, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      auth_token,
+      marketplace_token: process.env.SALEOR_MARKETPLACE_TOKEN,
+    }),
+  });
+
+  response.json({ success: true });
+};
+
+export default handler;
