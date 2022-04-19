7c328c04bddc28efbc8ca381694704387f40e9ed
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-04-19 19:11:31 +0200

    Remove unnecessary console.log

--- apps/checkout/src/lib/utils/validation.ts
+++ apps/checkout/src/lib/utils/validation.ts
@@ -75,7 +75,6 @@ export const useGetFormErrorsFromApiErrors = <TFormData>(): ((
       return {} as FieldErrors<TFormData>;
     }
 
-    console.log(666, "API ERRORS", apiErrors);
     return apiErrors.reduce((result, { field, code }) => {
       const errorCode = code.toLowerCase() as ValidationErrorCode;
 
