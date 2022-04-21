b1d3e6cb2131bd4853d7b2cec0885de84d89ca9e
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-04-21 17:07:55 +0200

    Add setting errors from user register mutation

--- apps/checkout/src/sections/CheckoutForm/useCheckoutFinalize.ts
+++ apps/checkout/src/sections/CheckoutForm/useCheckoutFinalize.ts
@@ -2,6 +2,7 @@ import { pay as payRequest } from "@/fetch";
 import { useCheckout } from "@/hooks/useCheckout";
 import { useFetch } from "@/hooks/useFetch";
 import { extractMutationErrors } from "@/lib/utils";
+import { useErrors } from "@/providers/ErrorsProvider";
 import { useAuth } from "@saleor/sdk";
 import { FormData } from "./types";
 
@@ -9,6 +10,7 @@ export const useCheckoutFinalize = () => {
   const { checkout } = useCheckout();
   const { register } = useAuth();
   const [, pay] = useFetch(payRequest, { opts: { skip: true } });
+  const { setApiErrors } = useErrors<FormData>("userRegister");
 
   const checkoutPay = async () => {
     const data = await pay({
@@ -23,13 +25,17 @@ export const useCheckoutFinalize = () => {
   };
 
   const checkoutFinalize = async (formData: FormData) => {
-    const result = await register(formData);
+    // adding redirect url becuase api is broken and requires it
+    // despite te types saying otherwise
+    const result = await register({ ...formData, redirectUrl: "" });
 
     const [hasErrors, errors] = extractMutationErrors(result);
 
     if (!hasErrors) {
       checkoutPay();
     }
+
+    setApiErrors(errors);
   };
 
   return { checkoutFinalize };
