faf2de0ddb428651907f61fc13de3de37511b0d5
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-04-27 12:28:52 +0200

    Add label component from ui kit

--- apps/checkout/src/components/Label.tsx
+++ apps/checkout/src/components/Label.tsx
@@ -1,20 +1,10 @@
-import { Classes } from "@/lib/globalTypes";
-import clsx from "clsx";
+import { Label as UiKitLabel } from "@saleor/ui-kit";
 import React, { PropsWithChildren } from "react";
 
-interface LabelProps extends Classes {
+interface LabelProps {
   htmlFor: string;
 }
 
-export const Label: React.FC<PropsWithChildren<LabelProps>> = ({
-  children,
-  htmlFor,
-  className,
-}) => (
-  <label
-    htmlFor={htmlFor}
-    className={clsx("text-text-primary text-base font-regular", className)}
-  >
-    {children}
-  </label>
+export const Label: React.FC<PropsWithChildren<LabelProps>> = ({ ...rest }) => (
+  <UiKitLabel {...rest} />
 );
