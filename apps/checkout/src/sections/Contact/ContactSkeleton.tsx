99c7452d087382e0e440579ef5cc5e223eef7096
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-05-05 14:53:01 +0200

    Improve contact skeleton

--- apps/checkout/src/sections/Contact/ContactSkeleton.tsx
+++ apps/checkout/src/sections/Contact/ContactSkeleton.tsx
@@ -1,15 +1,14 @@
 import { Skeleton } from "@/components/Skeleton";
 import React from "react";
 
-interface ContactSkeletonProps {}
-
-export const ContactSkeleton: React.FC<ContactSkeletonProps> = ({}) => {
+export const ContactSkeleton: React.FC = () => {
   return (
     <div>
-      <Skeleton variant="title" />
+      <Skeleton variant="title" className="w-1/3" />
       <div className="flex flex-row justify-between">
-        <Skeleton className="w-1/2" />
-        <Skeleton className="w-1/8" />
+        <Skeleton className="w-3/4" />
+        <div className="skeleton-horizontal-spacer w-1/2" />
+        <Skeleton className="w-1/4" />
       </div>
     </div>
   );
