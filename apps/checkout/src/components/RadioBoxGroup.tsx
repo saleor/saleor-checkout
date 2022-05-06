8fe6ad0d75acb25f0b29c8b15735550242f4812d
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-05-06 09:54:39 +0200

    Add radio group accessibility props

--- apps/checkout/src/components/RadioBoxGroup.tsx
+++ apps/checkout/src/components/RadioBoxGroup.tsx
@@ -8,5 +8,9 @@ export const RadioBoxGroup: React.FC<PropsWithChildren<RadioBoxGroupProps>> = ({
   label,
   children,
 }) => {
-  return <div className="radio-box-group">{children}</div>;
+  return (
+    <div role="radiogroup" aria-label={label} className="radio-box-group">
+      {children}
+    </div>
+  );
 };
