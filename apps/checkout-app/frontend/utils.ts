e0e6d8465e5dcf1272c02e51d30e2facd380a793
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-05-09 16:55:59 +0200

    Remove JWT parse function

--- apps/checkout-app/frontend/utils.ts
+++ apps/checkout-app/frontend/utils.ts
@@ -16,22 +16,6 @@ import reduce from "lodash-es/reduce";
 import { CombinedError } from "urql";
 import { getSettingsPublicAccess } from "@/config/fields";
 
-export function parseJwt(token: string) {
-  var base64Url = token.split(".")[1];
-  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  var jsonPayload = decodeURIComponent(
-    Buffer.from(base64, "base64")
-      .toString()
-      .split("")
-      .map(function (c) {
-        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-      })
-      .join("")
-  );
-
-  return JSON.parse(jsonPayload);
-}
-
 export const flattenSettingId = (optionIdx: number, settingId: string) =>
   `${optionIdx}-${settingId}`;
 
