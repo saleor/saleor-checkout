3499cb73c4870ce742f8839f01fc80c977b75328
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-05-13 12:44:14 +0200

    Update app environment variables

--- apps/checkout-app/constants.ts
+++ apps/checkout-app/constants.ts
@@ -1,8 +1,24 @@
-export const APP_DOMAIN = process.env.NEXT_PUBLIC_VERCEL_URL;
-export const APP_URL = `https://${APP_DOMAIN}`;
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
-export const APP_NAME = "Checkout";
-export const SALEOR_DOMAIN_HEADER = "x-saleor-domain";
-export const SALEOR_TOKEN_HEADER = "x-saleor-token";
+export const appName = "Checkout";
 
 export const isSsr = typeof window === "undefined";
+export const saleorDomainHeader = "x-saleor-domain";
+export const saleorTokenHeader = "x-saleor-token";
+
+export type EnvVar = "appDomain" | "appUrl" | "appId" | "apiUrl";
+export type ServerEnvVar = "appToken";
+
+export type EnvVars = Record<EnvVar, string>;
+export type ServerEnvVars = Record<ServerEnvVar, string>;
+
+const env = process.env;
+
+export const envVars: EnvVars = {
+  appDomain: env.NEXT_PUBLIC_VERCEL_URL!,
+  appUrl: `https://${env.NEXT_PUBLIC_VERCEL_URL!}`,
+  appId: env.SALEOR_APP_ID!,
+  apiUrl: env.NEXT_PUBLIC_API_URL!,
+} as EnvVars;
+
+export const serverEnvVars: ServerEnvVars = {
+  appToken: env.SALEOR_APP_TOKEN,
+} as ServerEnvVars;
--- apps/checkout-app/pages/payment-providers/[paymentProviderId].tsx
+++ apps/checkout-app/pages/payment-providers/[paymentProviderId].tsx
@@ -15,6 +15,7 @@ import { usePaymentProviderSettings } from "@/frontend/data";
 import ErrorDetails from "@/frontend/components/templates/ErrorDetails";
 import { useIntl } from "react-intl";
 import { notFoundMessages } from "@/frontend/misc/errorMessages";
+import { envVars } from "@/constants";
 
 const PaymentProvider = () => {
   const router = useRouter();
@@ -24,7 +25,7 @@ const PaymentProvider = () => {
   const { appId, isAuthorized } = useAuthData();
   const [metadataQuery] = usePrivateMetadataQuery({
     variables: {
-      id: appId,
+      id: appId || envVars.appId,
     },
     pause: !isAuthorized,
   });
@@ -55,7 +56,7 @@ const PaymentProvider = () => {
     });
 
     setPrivateMetadata({
-      id: appId,
+      id: appId || envVars.appId,
       input: metadata,
     });
   };
