2c27f106279ec8e47b48618aef5728b878232fac
Jakub Majorek <majorek.jakub@gmail.com>
2022-06-09 18:42:11 +0200

    Reuse payment session

---
+++ apps/checkout-app/backend/payments/providers/adyen/verifySession.ts
@@ -0,0 +1,25 @@
+import { CheckoutAPI, Client } from "@adyen/api-library";
+
+import { getPrivateSettings } from "@/checkout-app/backend/configuration/settings";
+import { envVars } from "@/checkout-app/constants";
+
+export const verifySession = async (session: string) => {
+  const {
+    paymentProviders: { adyen },
+  } = await getPrivateSettings(envVars.apiUrl, false);
+
+  if (!adyen.apiKey) {
+    throw "API key not defined";
+  }
+
+  const client = new Client({
+    apiKey: adyen.apiKey,
+    environment: "TEST",
+  });
+
+  const checkout = new CheckoutAPI(client);
+
+  const { status, url } = await checkout.getPaymentLinks(session);
+
+  return { status, url };
+};
