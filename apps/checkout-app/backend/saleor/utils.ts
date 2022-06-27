2d71e0dbb415e54791576b902cafec918fffe77d
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-06-27 17:05:56 +0200

    Validate Saleor webhook calls using JWK

--- apps/checkout-app/backend/saleor/utils.ts
+++ apps/checkout-app/backend/saleor/utils.ts
@@ -1,15 +1,37 @@
 import { NextApiRequest } from "next";
 import * as jose from "jose";
+import crypto from "crypto";
 import { envVars, serverEnvVars } from "@/checkout-app/constants";
 
+const getSaleorDomain = () => {
+  const url = new URL(envVars.apiUrl);
+  return url.origin;
+};
+
 const JWKS = jose.createRemoteJWKSet(
-  new URL(envVars.apiUrl + "/.well-known/jwks.json")
+  new URL(getSaleorDomain() + "/.well-known/jwks.json")
 );
 
-export function validateSaleorRequest(
-  rawBody: string,
+export async function isValidSaleorRequest(
+  bodyBuffer: Buffer,
   signature: string
-): boolean {
-  // TODO: Add validation
-  return true;
+): Promise<boolean> {
+  // TODO: Use JWS (JSON Web Signature) when implemented in core
+
+  let key;
+  try {
+    // @ts-expect-error using fake JWS to get the key
+    key = await JWKS({ alg: "RS256", kid: "1" }, {});
+  } catch (e) {
+    console.warn("Error while getting Saleor JWK", e);
+    return false;
+  }
+
+  return crypto.verify(
+    "rsa-sha256",
+    bodyBuffer,
+    // @ts-expect-error mismatch between node types and jose
+    key,
+    Buffer.from(signature, "hex")
+  );
 }
--- apps/checkout-app/pages/api/webhooks/saleor/transaction-action-request.ts
+++ apps/checkout-app/pages/api/webhooks/saleor/transaction-action-request.ts
@@ -5,7 +5,7 @@ import { TransactionActionPayloadFragment } from "@/checkout-app/graphql";
 import { NextApiRequest, NextApiResponse } from "next";
 import getRawBody from "raw-body";
 import contentType from "content-type";
-import { validateSaleorRequest } from "@/checkout-app/backend/saleor/utils";
+import { isValidSaleorRequest } from "@/checkout-app/backend/saleor/utils";
 
 export const config = {
   api: {
@@ -21,33 +21,31 @@ export default async function handler(
     res.setHeader("Allow", "POST");
     res.status(405).end("Method Not Allowed");
   }
-  debugger;
   const buf = await getRawBody(req, {
     length: req.headers["content-length"],
     limit: "1mb",
-    encoding: contentType.parse(req).parameters.charset,
   });
-  const rawBody = buf.toString();
-  let body: TransactionActionPayloadFragment;
 
   const signature = req.headers["saleor-signature"];
 
   if (typeof signature !== "string" || signature.length === 0) {
-    res.status(400).end("Invalid request");
+    res.status(401).end("Unathorized");
     console.error("No Saleor signature present in request");
     return;
   }
 
-  if (!validateSaleorRequest(rawBody, signature)) {
-    res.status(400).end("Invalid request");
+  if (!(await isValidSaleorRequest(buf, signature))) {
+    res.status(401).end("Unauthorized");
     console.error("Invalid Saleor signature", signature);
     return;
   }
 
+  let body: TransactionActionPayloadFragment;
+
   try {
-    body = JSON.parse(rawBody);
+    body = JSON.parse(buf.toString());
   } catch (e) {
-    res.status(400).end("Request data is not JSON");
+    res.status(400).end("Request data is not valid JSON");
     return;
   }
 
