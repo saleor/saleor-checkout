2739fa8bee32ff13650cc5778282216e222e676c
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-06-29 10:38:51 +0200

    Refactor api url generation to use url-join

--- apps/checkout/src/fetch/requests.ts
+++ apps/checkout/src/fetch/requests.ts
@@ -5,6 +5,7 @@ import { PayRequestBody } from "checkout-app/types/api/pay";
 import { PaymentStatusResponse } from "checkout-app/types/api/payment-status";
 import { PayResult } from "./types";
 import { ChannelActivePaymentProvidersByChannel } from "checkout-app/types";
+import urlJoin from "url-join";
 
 export type PaymentMethodsRequestArgs = {
   channelId: string;
@@ -13,20 +14,20 @@ export type PaymentMethodsRequestArgs = {
 export const getPaymentMethods = ({
   channelId,
 }: PaymentMethodsRequestArgs): FetchResponse<ChannelActivePaymentProvidersByChannel> =>
-  fetch(`${envVars.checkoutApiUrl}/active-payment-providers/${channelId}`);
+  fetch(urlJoin(envVars.checkoutApiUrl, "active-payment-providers", channelId));
 
 export const pay = (body: PayRequestBody): FetchResponse<PayResult> =>
-  fetch(`${envVars.checkoutApiUrl}/pay`, {
+  fetch(urlJoin(envVars.checkoutApiUrl, "pay"), {
     method: "POST",
     body: JSON.stringify(body),
   });
 
 export const getAppConfig = (): FetchResponse<AppConfig> =>
-  fetch(`${envVars.checkoutApiUrl}/customization-settings`);
+  fetch(urlJoin(envVars.checkoutApiUrl, "customization-settings"));
 
 export const getOrderPaymentStatus = ({
   orderId,
 }: {
   orderId: string;
 }): FetchResponse<PaymentStatusResponse> =>
-  fetch(`${envVars.checkoutApiUrl}/payment-status/${orderId}`);
+  fetch(urlJoin(envVars.checkoutApiUrl, "payment-status", orderId));
