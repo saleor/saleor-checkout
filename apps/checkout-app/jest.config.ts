cfa92142f8d98f137820c704814064ec30a6eb29
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-05 14:59:17 +0200

    Fix Jest config for making fetch requests

--- apps/checkout-app/jest.config.ts
+++ apps/checkout-app/jest.config.ts
@@ -64,9 +64,9 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    fetch: () => undefined,
-  },
+  // globals: {
+  //   fetch: () => undefined,
+  // },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -136,10 +136,10 @@ export default {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["./setupTests"],
+  setupFiles: ["./setupTestsBeforeEnv"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["./setupTestsAfterEnv"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
