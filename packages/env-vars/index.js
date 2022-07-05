32baa4cf783fa95682cf695dc256eb7b3074a2aa
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-05 14:57:06 +0200

    Load env variables from .env.local in env-vars package

--- packages/env-vars/index.js
+++ packages/env-vars/index.js
@@ -1,10 +1,16 @@
 const dotenv = require("dotenv");
 const dotenvExpand = require("dotenv-expand");
 const findConfig = require("find-config");
+const fs = require("fs");
 
 // Load env variables from root of monorepo
 dotenv.config({ path: findConfig(".env", { cwd: ".." }) });
 // Load env variables from apps/checkout
 const env = dotenv.config();
+
+if (fs.statSync(".env.local").isFile()) {
+  dotenv.config({ path: ".env.local" });
+}
+
 // Replace $ in .env with loaded env variables
 dotenvExpand.expand(env);
