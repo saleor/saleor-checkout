d405706d98c98d745aa08bdad329f927866f085a
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-11 10:45:53 +0200

    Fix env variables apply order in packages/env-vars

--- packages/env-vars/index.js
+++ packages/env-vars/index.js
@@ -3,14 +3,20 @@ const dotenvExpand = require("dotenv-expand");
 const findConfig = require("find-config");
 const fs = require("fs");
 
-// Load env variables from root of monorepo
-dotenv.config({ path: findConfig(".env", { cwd: ".." }) });
-// Load env variables from apps/checkout
-const env = dotenv.config();
+// Note - env variables don't override each other
+// if they have been loaded, so the order of loading is flipped from
+// default next.js behavior (first .env.loca, then .env)
 
+// Load env variables from apps/**/.env.local
 if (fs.statSync(".env.local").isFile()) {
   dotenv.config({ path: ".env.local" });
 }
+
+// Load env variables from apps/**/.env
+const env = dotenv.config();
+
+// Load env variables from root of monorepo
+dotenv.config({ path: findConfig(".env", { cwd: ".." }) });
 
 // Replace $ in .env with loaded env variables
 dotenvExpand.expand(env);
