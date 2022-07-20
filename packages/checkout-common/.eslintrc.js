651f8134883df57d8992db0763c275b95b8cb673
Magdalena Markusik <magdalena.markusik@mirumee.com>
2022-07-20 13:48:22 +0200

    Fix eslint

--- packages/checkout-common/.eslintrc.js
+++ packages/checkout-common/.eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
   },
+<<<<<<< HEAD
   plugins: [],
   extends: ["checkout"],
   ignorePatterns: "*.js",
+=======
+  extends: ["checkout"],
+  ignorePatterns: [".eslintrc.js"],
+>>>>>>> Fix eslint
 };
