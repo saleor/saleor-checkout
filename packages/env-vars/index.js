9f292968f6af6ba9d1eb81639fa50011524502a5
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-21 15:24:06 +0200

    Fix env loading in tests when .env.local doesn't exist

--- packages/env-vars/index.js
+++ packages/env-vars/index.js
@@ -8,17 +8,23 @@ const fs = require("fs");
 // default next.js behavior (first .env.loca, then .env)
 
 // Load env variables from apps/**/.env.local
-if (fs.statSync(".env.local").isFile()) {
-  dotenv.config({ path: ".env.local" });
+let localEnv;
+if (fs.existsSync(".env.local")) {
+  localEnv = dotenv.config({ path: ".env.local" });
 }
 
 // Load env variables from apps/**/.env
 const env = dotenv.config();
 
 // Load env variables from root of monorepo
-dotenv.config({ path: findConfig(".env.local", { cwd: ".." }) });
+const rootEnvLocal = findConfig(".env.local", { cwd: ".." });
+if (fs.existsSync(rootEnvLocal)) {
+  dotenv.config({ path: rootEnvLocal });
+}
 dotenv.config({ path: findConfig(".env", { cwd: ".." }) });
 
-// Replace $ in .env with loaded env variables
-dotenvExpand.expand(dotenv.config({ path: ".env.local" }));
-dotenvExpand.expand(dotenv.config({ path: ".env" }));
+// Replace $ in .env and .env.local with loaded env variables from root of monorepo
+if (localEnv) {
+  dotenvExpand.expand(localEnv);
+}
+dotenvExpand.expand(env);
