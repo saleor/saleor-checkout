a9a77c6104903fd77b5b1fbb1a59e891eeb5fcc9
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-07-26 15:46:06 +0200

    Fix envs not loading in constants

--- apps/saleor-app-checkout/constants.ts
+++ apps/saleor-app-checkout/constants.ts
@@ -21,14 +21,13 @@ export const envVarsNames: EnvVars = {
   apiUrl: "NEXT_PUBLIC_SALEOR_API_URL",
 };
 
+export const serverEnvVars: ServerEnvVars = {
+  appToken: process.env["SALEOR_APP_TOKEN"]!,
+  settingsEncryptionSecret: process.env["SETTINGS_ENCRYPTION_SECRET"],
+};
 export const serverEnvVarNames: Record<ServerEnvVar, string> = {
   appToken: "SALEOR_APP_TOKEN",
   settingsEncryptionSecret: "SETTINGS_ENCRYPTION_SECRET",
-};
-export const serverEnvVars: ServerEnvVars = {
-  appToken: process.env[serverEnvVarNames.appToken]!,
-  settingsEncryptionSecret:
-    process.env[serverEnvVarNames.settingsEncryptionSecret],
 };
 
 export const debugEnvVars: DebugEnvVars | null =
